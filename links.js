18183ab77d50eee330a2c321480d4c213bf93a6c
cqs0918 <abcd6314581@gmail.com>
2023-02-05 15:38:27 +0800

    2/5 modify

---
+++ links.js
@@ -0,0 +1,13 @@
+function jumpToInstagram(){
+    window.location = "https://www.instagram.com/cqs_0918__/";
+}
+function jumpToFacebook(){
+    window.location = "https://www.facebook.com/qiu.sing.3/";
+}
+function jumpToGithub(){
+    window.location = "https://github.com/cqs0918"
+}
+function backToPreviousPage (){
+    console.log("back");
+    window.history.back();
+}
